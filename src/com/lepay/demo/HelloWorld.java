94eff5e142052c56740e61764cf8aae259729b7e
xuxiaowen <xuxiaowen@qq.com>
2020-06-07 22:11:56 +0800

    用户修改了代码

--- src/com/lepay/demo/HelloWorld.java
+++ src/com/lepay/demo/HelloWorld.java
@@ -3,6 +3,8 @@ package com.lepay.demo;
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.printf("Hello World!");
-        System.out.printf("用户1修改了代码---》aaa");
+        System.out.printf("用户1修改了代码 ---> aaaa");
+        System.out.printf("用户1再次修改了代码 ---> bbbb");
+        System.out.printf("用户1再次修改了代码 ---> cccc");
     }
 }
